bbfc6843c8435fa184359ad4651b696004a2484e
Lowe <sonny.lowe55@gmail.com>
2020-08-31 18:49:45 -0700

    counter added

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,8 @@
 let addReadingButton = document.getElementById("addReadingButton");
 let bookList = document.getElementById("bookList");
+let counter = 0;
 
 addReadingButton.onclick = function(){
-    bookList.innerHTML += "<p>bobby</p>";
+    bookList.innerHTML += "<p>bobby" + counter + "</p>";
+    counter++;
 }
